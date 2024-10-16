5cd84277f83a9bee41c4ddd8f07092fba9c44cca
Your Nameecho <you@example.com>
2024-10-16 21:24:32 +0530

    MERN Learning Management System project initialized structure

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import { mongoDbConnection } from './utils/db.js';
+
+// dotenv configuration
+config();
+
+const { PORT = 5000, CLIENT_URL } = process.env;
+const app = express();
+
+// middlewares
+app.use(cors({
+    origin: CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.use(express.json());
+
+// routers configuration
+
+// error handling
+app.use((err, req, res, next) => {
+    console.log(err.stack || err);
+    res.status(500).json({
+        success: false,
+        message: "ERROR FROM SERVER, SOMETHING WENT WRONG!",
+        error: err.stack || err
+    });
+});
+
+// mongodb connection & server listening
+mongoDbConnection()
+    .then(() => {
+        app.listen(PORT, (err) => {
+            if (err) console.error(err);
+            console.log(`Server listening on ${PORT}`);
+        })
+    }).catch((err) => {
+        console.error(err);
+    });
+
+// next time work time stamp :: 30:49 Client side basic pages and routing implementation
