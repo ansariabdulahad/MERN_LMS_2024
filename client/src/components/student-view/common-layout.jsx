3d931cecf657f987baca54726e9bd7f704f0a521
Your Nameecho <you@example.com>
2024-10-31 17:40:15 +0530

    student panel header, banner, and categories added

--- client/src/components/student-view/common-layout.jsx
+++ client/src/components/student-view/common-layout.jsx
@@ -1,31 +1,12 @@
-import { AuthContext } from '@/context/auth-context'
-import React, { useContext } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Button } from '../ui/button';
+import StudentViewCommonHeader from './header';
 
 const StudentViewCommonLayout = () => {
 
-    const { resetCredentials } = useContext(AuthContext);
-
-    // handle logout
-    const handleLogout = (event) => {
-        const logoutBtn = event.target;
-
-        logoutBtn.innerHTML = "Logging out...";
-        logoutBtn.disabled = true;
-
-        resetCredentials();
-
-        // logoutBtn.innerHTML = "Logout";
-        // logoutBtn.disabled = false;
-    }
-
     return (
         <div>
-            Common Content
-            <Button
-                onClick={handleLogout}
-            >Logout</Button>
+            <StudentViewCommonHeader />
             <Outlet />
         </div>
     )
--- client/src/pages/student/home/index.jsx
+++ client/src/pages/student/home/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import banner from '../../../../public/lms-banner.png';
+import { courseCategories } from '@/config';
+import { Button } from '@/components/ui/button';
 
 const StudentHomePage = () => {
     return (
-        <div>HomePage</div>
+        <div className='min-h-screen bg-white'>
+            <section className='flex flex-col lg:flex-row items-center justify-between py-8 px-4 lg:px-8'>
+                <div className='lg:w-1/2 lg:pr-12'>
+                    <h1 className='text-3xl sm:text-5xl font-bold mb-4 capitalize'>Learning that gets you!</h1>
+                    <p className='text-xl'>Skills for your present and your feature. Get started with us!</p>
+                </div>
+
+                <div className='lg:w-full mb-8 lg:mb-0'>
+                    <img
+                        src={banner}
+                        alt="banner-img"
+                        width={600}
+                        height={400}
+                        className='w-full h-auto rounded-lg shadow-lg'
+                    />
+                </div>
+            </section>
+
+            <section className='py-8 px-4 lg:px-8 bg-gray-100'>
+                <h2 className='text-2xl font-bold mb-6'>Course Categories</h2>
+
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
+                    {
+                        courseCategories.map((categoryItem) => (
+                            <Button
+                                key={categoryItem.id}
+                                className="justify-start"
+                                variant="outline"
+                            >{categoryItem.label}</Button>
+                        ))
+                    }
+                </div>
+            </section>
+        </div>
     )
 }
 
 export default StudentHomePage
+
+// next timestap: 6:50 - work on course listings
